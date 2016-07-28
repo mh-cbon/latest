6ee0b456a7441c21b6ad398ef885366da783e5f7
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-28 14:58:31 +0200

    windows: fix command detection

--- latest.bat
+++ latest.bat
@@ -10,7 +10,7 @@ gh-api-cli.exe --version >nul 2>&1 && (
 ) || (
   curl -fsSL -o C:\gh-api-cli.msi https://github.com/mh-cbon/gh-api-cli/releases/download/2.0.1/gh-api-cli-amd64.msi
   msiexec.exe /i C:\gh-api-cli.msi /quiet
-  set PATH=C:\Program Files\gh-api-cli\;%PATH%
+  set PATH="C:\Program Files\gh-api-cli\;%PATH%"
 )
 
 go-msi.exe --version >nul 2>&1 && (
@@ -18,5 +18,5 @@ go-msi.exe --version >nul 2>&1 && (
 ) || (
   gh-api-cli.exe dl-assets -o %USER% -r %REPO% -g "*%ARCH%*msi" --ver latest --out "C:\%REPO%.msi"
   msiexec.exe /i C:\%REPO%.msi /quiet
-  set PATH=C:\Program Files\%REPO%\;%PATH%
+  set PATH="C:\Program Files\%REPO%\;%PATH%"
 )
