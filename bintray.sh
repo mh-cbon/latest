ed00f88e3670f81cbc58ec7d88af8a8adc931337
mh-cbon <mh-cbon@users.noreply.github.com>
2017-08-27 16:09:00 +0200

    bintray: add helper installer

---
+++ bintray.sh
@@ -0,0 +1,99 @@
+#!/bin/sh -e
+
+# this is an helper
+# to install a source repo
+# hosted on bintray
+#
+# to use it
+# curl -L https://raw.githubusercontent.com/mh-cbon/latest/master/bintray.sh \
+# | GH=USER/REPO sh -xe
+
+# GH=$1
+# DISTRIB=$2
+# COMPONENT=$3
+
+if [ -z "$GH" ]; then
+    echo "wrong command line, must pass GH=user/repo"
+fi
+
+if [ -z "$DISTRIB" ]; then
+    DISTRIB="unstable"
+fi
+
+if [ -z "$COMPONENT" ]; then
+    COMPONENT="main"
+fi
+
+REPO=`echo ${GH} | cut -d '/' -f 2`
+USER=`echo ${GH} | cut -d '/' -f 1`
+
+DLCMD=""
+DLArgs=""
+FILE=""
+URL=""
+
+if type "dpkg" > /dev/null; then
+
+  echo "deb https://dl.bintray.com/${USER}/deb ${DISTRIB} ${COMPONENT}" | sudo tee -a /etc/apt/sources.list
+
+  if type "sudo" > /dev/null; then
+    sudo apt-get install apt-transport-https -y --quiet
+    sudo apt-get update --quiet
+    sudo apt-get install ${REPO} -y --quiet
+  else
+    apt-get install apt-transport-https -y --quiet
+    apt-get update --quiet
+    apt-get install ${REPO} -y --quiet
+  fi
+
+elif type "dnf" > /dev/null; then
+
+  FILE=/etc/yum.repos.d/${USER}.repo
+  URL=https://bintray.com/${USER}/rpm/rpm/${REPO}.repo
+
+  if type "wget" > /dev/null; then
+    DLCMD='wget -q -O '
+    DLArgs="${FILE} ${URL}"
+  elif type "curl" > /dev/null; then
+    DLCMD='curl -s -L'
+    DLArgs="${URL} > ${FILE}"
+  fi
+
+  if type "sudo" > /dev/null; then
+    sudo /bin/sh -c "${DLCMD} ${DLArgs}"
+  else
+    $DLCMD ${DLArgs}
+  fi
+
+  if type "sudo" > /dev/null; then
+    sudo dnf install ${REPO} -y --quiet
+  else
+    dnf install ${REPO} -y --quiet
+  fi
+
+elif type "yum" > /dev/null; then
+
+  FILE=/etc/yum.repos.d/${USER}.repo
+  URL=https://bintray.com/${USER}/rpm/rpm/${REPO}.repo
+
+  if type "wget" > /dev/null; then
+    DLCMD='wget -q -O '
+    DLArgs="${FILE} ${URL}"
+  elif type "curl" > /dev/null; then
+    DLCMD='curl -s -L'
+    DLArgs="${URL} > ${FILE}"
+  fi
+
+  if type "sudo" > /dev/null; then
+    sudo /bin/sh -c "${DLCMD} ${DLArgs}"
+  else
+    $DLCMD ${DLArgs}
+  fi
+
+  if type "sudo" > /dev/null; then
+    sudo yum install ${REPO} -y --quiet
+  else
+    yum install ${REPO} -y --quiet
+  fi
+
+fi
